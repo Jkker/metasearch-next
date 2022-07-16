1dd49b4a4b4924eecda48717992d22af8a427de7
jkker <me@jerrykjia.com>
2022-07-15 20:57:06 -0400

    feat: opensearch

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
 		<>
 			<Head>
 				<meta content='width=device-width, initial-scale=1' name='viewport' />
+				<link rel="search" type="application/opensearchdescription+xml" href="opensearch.xml" title="Metasearch" />
 			</Head>
 			<ThemeProvider attribute='class' defaultTheme='system'>
 				<Component {...pageProps} />
