7d1039f299aab2daa95a4c6c1200635b7267f47b
Jerry K Jia <j@erry.dev>
2022-08-20 03:14:45 -0400

    Fix: search bar add name="s"

--- pages/index.js
+++ pages/index.js
@@ -287,6 +287,7 @@ export default function Search({ engines, hotkeys: tabHotkeys }) {
 							className='w-full h-9 p-2 pl-9 bg-transparent'
 							id='search-input'
 						        type='search'
+						        name='s'
 						/>
 						<button
 							className='absolute top-0 left-0 h-9 w-9 flex-center'
